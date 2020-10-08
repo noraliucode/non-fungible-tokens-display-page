b778ad812f64c5d45f7cf84c1bc10a8985450e51
Nora Liu <tzuyuanliu0830@gmail.com>
2020-10-08 09:07:27 +0800

    feat: add list page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.opensea.io/api/v1/assets?owner=0x960DE9907A2e2f5363646d48D7FB675Cd2892e91&offset=0&limit=20')
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				const list = data.assets.map((item) => ({
+					imageUrl: item.image_original_url,
+					name: item.name,
+					collectionName: item.collection.name,
+					description: item.description,
+					permalink: item.permalink,
+				}));
+				setList(list);
+				console.log('list', list);
+			});
+	}, []);
+	return (
+		<div className='App'>
+			<div className='container'>
+				{list.map((item, index) => (
+					<div key={`${item.name}_${index}`}>
+						<img className='listImg' src={item.imageUrl} alt={'Item'} />
+						<div>{item.name}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
