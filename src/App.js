85f67839ea901ec5d5db5f2d40446a06edf72840
Nora Liu <tzuyuanliu0830@gmail.com>
2020-10-08 23:16:15 +0800

    feat: append items when user scroll to the bottom

--- src/App.js
+++ src/App.js
@@ -5,29 +5,24 @@ import './App.css';
 function App() {
 	const [list, setList] = useState([]);
 	const [isBottom, setIsBottom] = useState(false);
+	const [offset, setOffect] = useState(0);
 
 	useEffect(() => {
 		window.addEventListener('scroll', handleScroll);
-
-		fetch('https://api.opensea.io/api/v1/assets?owner=0x960DE9907A2e2f5363646d48D7FB675Cd2892e91&offset=0&limit=20')
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-				const list = data.assets.map((item) => ({
-					imageUrl: item.image_original_url,
-					name: item.name,
-					collectionName: item.collection.name,
-					description: item.description,
-					permalink: item.permalink,
-				}));
-				setList(list);
-				console.log('list', list);
-			});
+		fetchList();
 
 		return () => {
 			window.removeEventListener('scroll', handleScroll);
 		};
 	}, []);
+
+	useEffect(() => {
+		if (isBottom) {
+			const temp = offset + 20;
+			fetchList(temp);
+			setOffect(temp);
+		}
+	}, [isBottom]);
 
 	const handleScroll = () => {
 		const windowHeight = 'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight;
@@ -35,7 +30,7 @@ function App() {
 		const html = document.documentElement;
 		const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 		const windowBottom = windowHeight + window.pageYOffset;
-		if (windowBottom >= docHeight) {
+		if (windowBottom >= docHeight - 10) {
 			setIsBottom(true);
 			console.log('isBottom');
 		} else {
@@ -43,6 +38,26 @@ function App() {
 			console.log('notBottom');
 		}
 	};
+
+	const fetchList = (value = 0) => {
+		console.log('fetchList value', value);
+		fetch(`https://api.opensea.io/api/v1/assets?owner=0x960DE9907A2e2f5363646d48D7FB675Cd2892e91&offset=${value}&limit=20`)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				const result = data.assets.map((item) => ({
+					imageUrl: item.image_original_url,
+					name: item.name,
+					collectionName: item.collection.name,
+					description: item.description,
+					permalink: item.permalink,
+				}));
+				const temp = list.concat(result);
+				setList(temp);
+				console.log('list', list);
+			});
+	};
+
 	return (
 		<div className='App'>
 			<div className='container'>
